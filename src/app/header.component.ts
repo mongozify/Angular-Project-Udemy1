651ac72413dc529419f2f73c4d25eb2cb2985594
mongozify <v.cebotaev.it@gmail.com>
2025-09-03 00:13:02 +0200

    Erste Kurscommit

---
+++ src/app/header.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-header',
+    standalone: true,
+    templateUrl: './header.component.html',
+    // styles: '[  h1 { color: blue } ]', ----> binding styles directly in component file   
+    // styleUrl: './header.component.css', ----> single file for styles
+    // styleUrls: './header.component.css', --> Array of multiple files for styles
+    styleUrls: ['./header.component.scss']
+})
+
+export class HeaderComponent {
+
+}
