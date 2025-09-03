e3b5697f16bc2f391de317e792016592173160c7
vch <vch@connext.de>
2025-09-03 16:18:53 +0200

    Dritte Commit. Import, New Componenten

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,12 +4,10 @@ import { Component } from "@angular/core";
     selector: 'app-header',
     standalone: true,
     templateUrl: './header.component.html',
-    // styles: '[  h1 { color: blue } ]', ----> binding styles directly in component file   
+    // styles: '[  h1 { color: blue } ]', ----> binding styles directly in component file
     // styleUrl: './header.component.css', ----> single file for styles
     // styleUrls: './header.component.css', --> Array of multiple files for styles
     styleUrls: ['./header.component.scss']
 })
 
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  standalone: true,
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.scss'
+})
+export class UserComponent {
+  selectedUser = DUMMY_USERS[randomIndex];
+
+}
